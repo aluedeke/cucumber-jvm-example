5f79be99eb57208170a2d467fef67e033a3bb8d5
aluedeke <aluedeke@testobject.org>
2012-03-29 08:07:13 +0200

    updating cucumber to latest version removing unnecessary pico dependencies moving feature to right package

--- src/test/java/net/ludeke/example/ATMTest.java
+++ src/test/java/net/ludeke/example/ATMTest.java
@@ -3,11 +3,8 @@ package net.ludeke.example;
 import org.junit.runner.RunWith;
 
 import cucumber.junit.Cucumber;
-import cucumber.junit.Feature;
 
 @RunWith(Cucumber.class)
-@Feature(value = "ATM.feature")
 public class ATMTest {
-	
-	
+
 }
